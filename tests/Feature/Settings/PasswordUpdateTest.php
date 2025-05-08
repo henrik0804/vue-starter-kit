45e15b752731fc5c57025629e5a270467b98d177
henrik0804 <henrik.schumacher@cre8-it.de>
2025-05-08 18:48:09 +0200

    increase test coverage to 100

--- tests/Feature/Settings/PasswordUpdateTest.php
+++ tests/Feature/Settings/PasswordUpdateTest.php
@@ -4,14 +4,29 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Settings;
 
+use App\Http\Requests\Auth\LoginRequest;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Validation\ValidationException;
+use Inertia\Testing\AssertableInertia as Assert;
 use Tests\TestCase;
 
 final class PasswordUpdateTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function test_password_update_screen_is_rendered(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('password.edit'));
+
+        $response->assertStatus(200)->assertInertia(fn(Assert $page) => $page
+            ->component('settings/Password')
+        );
+    }
 
     public function test_password_can_be_updated(): void
     {
@@ -49,5 +64,28 @@ final class PasswordUpdateTest extends TestCase
         $response
             ->assertSessionHasErrors('current_password')
             ->assertRedirect('/settings/password');
+    }
+
+    public function test_validation_exception_thrown_when_too_many_login_attempts_occur(): void
+    {
+        $request = new LoginRequest();
+
+        $throttleKey = $request->throttleKey();
+
+        RateLimiter::clear($throttleKey);
+
+        foreach (range(1, 5) as $_) {
+            RateLimiter::hit($throttleKey);
+        }
+
+        $this->expectException(ValidationException::class);
+        $this->expectExceptionMessage(trans('auth.throttle', [
+            'seconds' => RateLimiter::availableIn($throttleKey),
+            'minutes' => ceil(RateLimiter::availableIn($throttleKey) / 60),
+        ]));
+
+        $request->ensureIsNotRateLimited();
+
+        RateLimiter::clear($throttleKey);
     }
 }
